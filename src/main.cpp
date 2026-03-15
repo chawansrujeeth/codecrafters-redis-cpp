e3afd98e4521ff9afee80536ba7cb65cb44c50fb
chawansrujeeth <chawan22141@iiitd.ac.in>
2026-03-15 17:40:33 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -52,12 +52,12 @@ int main(int argc, char **argv) {
 
   // Uncomment the code below to pass the first stage
   
-  int clinet_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
 
   const char *response = "+PONG\r\n";
   send(client_fd, response, strlen(response), 0);
-  
+
   close(client_fd);
   close(server_fd);
 
