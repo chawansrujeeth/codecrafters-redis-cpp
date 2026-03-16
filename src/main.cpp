6d8c644f825a5e56edfb83bb72d8727a7a9f0adf
chawansrujeeth <chawan22141@iiitd.ac.in>
2026-03-16 22:06:40 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -38,33 +38,35 @@ int main(int argc, char **argv) {
     cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-  
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    cerr << "listen failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  cout << "Waiting for a client to connect...\n";
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  cout << "Logs from your program will appear here!\n";
-
-  // Uncomment the code below to pass the first stage
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  const char *response = "+PONG\r\n";
-  char buffer[1024];
   while(true){
-    size_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-    if (bytes_received <=0 ){
-      break;
+    
+    if (listen(server_fd, connection_backlog) != 0) {
+      cerr << "listen failed\n";
+      return 1;
     }
-    cout << "Client connected\n";
-    send(client_fd, response, strlen(response), 0);
+    
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    cout << "Waiting for a client to connect...\n";
+
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    cout << "Logs from your program will appear here!\n";
+
+    // Uncomment the code below to pass the first stage
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    const char *response = "+PONG\r\n";
+    char buffer[1024];
+    while(true){
+      size_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+      if (bytes_received <=0 ){
+        break;
+      }
+      cout << "Client connected\n";
+      send(client_fd, response, strlen(response), 0);
+    }
+    close(client_fd);
   }
-  close(client_fd);
 
   close(server_fd);
 
