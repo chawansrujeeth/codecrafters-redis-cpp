340496e1d9c6af6dd6a54c30c88df60a358db68f
chawansrujeeth <chawan22141@iiitd.ac.in>
2026-03-16 19:16:17 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,16 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+using namespace std;
+
 int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  // Flush after every cout / cerr
+  cout << unitbuf;
+  cerr << unitbuf;
   
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
+   cerr << "Failed to create server socket\n";
    return 1;
   }
   
@@ -23,7 +25,7 @@ int main(int argc, char **argv) {
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
+    cerr << "setsockopt failed\n";
     return 1;
   }
   
@@ -33,37 +35,36 @@ int main(int argc, char **argv) {
   server_addr.sin_port = htons(6379);
   
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
+    cerr << "Failed to bind to port 6379\n";
     return 1;
   }
   
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
+    cerr << "listen failed\n";
     return 1;
   }
   
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+  cout << "Waiting for a client to connect...\n";
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+  cout << "Logs from your program will appear here!\n";
 
   // Uncomment the code below to pass the first stage
-  
-  
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  const char *response = "+PONG\r\n";
+  char buffer[1024];
   while(true){
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-
-    std::cout << "Client connected\n";
-
-    const char *response = "+PONG\r\n";
+    size_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+    if (bytes_received <=0 ){
+      breeak;
+    }
+    cout << "Client connected\n";
     send(client_fd, response, strlen(response), 0);
-    close(client_fd);
-  
-
   }
+  close(client_fd);
 
   close(server_fd);
 
