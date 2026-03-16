015af9e17b44f38824dd1044f6b1555b1b94a93c
chawansrujeeth <chawan22141@iiitd.ac.in>
2026-03-16 19:09:49 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -36,26 +36,24 @@ int main(int argc, char **argv) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-
   
   int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+  
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+  std::cout << "Waiting for a client to connect...\n";
+
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  std::cout << "Logs from your program will appear here!\n";
+
+  // Uncomment the code below to pass the first stage
+  
+  
   while(true){
-    if (listen(server_fd, connection_backlog) != 0) {
-      std::cerr << "listen failed\n";
-      return 1;
-    }
-    
-    struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
-    std::cout << "Waiting for a client to connect...\n";
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
-
-    // Uncomment the code below to pass the first stage
-  
-  
-  
     int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
     std::cout << "Client connected\n";
