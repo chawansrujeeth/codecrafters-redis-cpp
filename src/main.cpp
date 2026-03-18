274af9903bc252a069f79b1c6b93cade05858215
chawansrujeeth <chawan22141@iiitd.ac.in>
2026-03-18 16:47:01 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -195,6 +195,22 @@ void handle_client(int client_fd){
           }
           string response = ":" + to_string(temp.size()) + "\r\n";
           send(client_fd,response.c_str(),response.length(),0);
+        }else if(command == "LPOP"){
+          string arg1 = args[1];
+          vector<string> temp;
+          if(set_list_store.count(arg1)){
+            temp = set_list_store[arg1];  
+          }
+          string value;
+          if(temp.size() > 0){
+            value = temp[0];
+            temp.erase(temp.begin());
+            set_list_store[arg1] = temp;
+          }else{
+            value = "";
+          }
+          string response = value.empty() ? "$-1\r\n" : "$" + to_string(value.length()) + "\r\n" + value + "\r\n";
+          send(client_fd,response.c_str(),response.length(),0);
         }
     }
   }
